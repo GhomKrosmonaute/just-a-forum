079a45f63a1cdc89b91229a7858ad534e95aad23
CamilleAbella <camille.abella@hotmail.fr>
2020-10-11 21:06:19 +0200

    add /admin route

---
+++ src/routes/admin.ts
@@ -0,0 +1,13 @@
+import app from "../server"
+import * as utils from "../utils"
+import * as entities from "../entities"
+
+app.get("/admin", function (req, res) {
+  utils.checkoutSession(req, res, (user) => {
+    if (!user.admin) {
+      return utils.error(res, "Access denied.")
+    }
+
+    res.render("pages/admin")
+  })
+})
