c575eb8f910f561d96330c8946250a65bba9f946
CamilleAbella <camille.abella@hotmail.fr>
2020-11-04 15:05:47 +0100

    renamed link routes to friendRequest

--- src/routes/friendRequest.ts
+++ src/routes/friendRequest.ts
@@ -2,7 +2,7 @@ import app from "../server"
 import * as entities from "../entities"
 import * as utils from "../utils"
 
-app.get("/link/:user_id", function (req: any, res: any) {
+app.get("/friendRequest/:user_id", function (req: any, res: any) {
   utils.checkoutSession(req, res, async (user) => {
     const target_id = req.params.user_id
     const target = await entities.User.fromId(target_id)
@@ -11,20 +11,20 @@ app.get("/link/:user_id", function (req: any, res: any) {
       return utils.error(res, "Unknown target...")
     }
 
-    const userLink = await entities.FriendRequest.find(
+    const userFriendRequest = await entities.FriendRequest.find(
       "author_id = ? AND target_id = ?",
       [user.id, target.id]
     )
-    const targetLink = await entities.FriendRequest.find(
+    const targetFriendRequest = await entities.FriendRequest.find(
       "author_id = ? AND target_id = ?",
       [target.id, user.id]
     )
 
-    if (userLink && targetLink) {
-      await userLink.delete()
-      await targetLink.delete()
-    } else if (userLink) {
-      await userLink.delete()
+    if (userFriendRequest && targetFriendRequest) {
+      await userFriendRequest.delete()
+      await targetFriendRequest.delete()
+    } else if (userFriendRequest) {
+      await userFriendRequest.delete()
     } else {
       await entities.FriendRequest.db.push({
         author_id: user.id,
